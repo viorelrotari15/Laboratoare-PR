d133d94598fe992c9c932b0d1702483630ba7e56
Viorel Rotari <nicoleta.gaina@ati.utm>
2020-04-23 19:25:10 +0300

    first commit

---
+++ Controllers/SendMailController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SendMailTO.Models;
+using System.Net.Mail;
+
+
+namespace SendMailTO.Controllers
+{
+    public class SendMailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Email em)
+        {
+            string to = em.To;
+            string subject = em.Subject;
+            string body = em.Body;
+
+            MailMessage mm = new MailMessage();
+
+            mm.To.Add(to);
+            mm.Subject = subject;
+            mm.Body = body;
+            mm.From = new MailAddress("viorelrotari15@gmail.com");
+            mm.IsBodyHtml = false;
+
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+            smtp.Port = 587;
+            smtp.UseDefaultCredentials = true;
+            smtp.EnableSsl = true;
+            smtp.Credentials = new System.Net.NetworkCredential("viorelrotari15@gmail.com", "069716871");
+            smtp.Send(mm);
+            ViewBag.message = "Mesajul este trimis catre " + em.To + "cu succes";
+
+            return View();
+        }
+    }
+}
